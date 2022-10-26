ff97591a79c94a36c0bceac3e729743d44cfeb67
StrahiCraft <strahi04@gmail.com>
2022-10-26 11:19:39 +0200

    Update 1
    -added the bird
    -added gravity to the player
    -added jumping to the player
    -added player rotation based on velocity

---
+++ Scripts/PlayerScipt.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+export (int) var fallingSpeed
+export (int) var jumpHeight
+export (float) var rotationStrength
+
+var velocity = Vector2()
+
+func _physics_process(delta):
+	_rotatePlayer(delta)
+	_movePlayer(delta)
+	pass
+
+func _process(delta):	
+	_updateVelocity(delta)
+	
+	if Input.is_action_just_pressed("jump"):
+		_jump()
+	pass
+	
+func _jump():
+	velocity.y = -jumpHeight
+	$AnimatedSprite.stop()
+	$AnimatedSprite.play("flap")
+	pass
+
+func _updateVelocity(delta):
+	velocity.y += fallingSpeed * delta
+	pass
+
+func _movePlayer(delta):
+	move_and_collide(velocity * delta)
+	pass
+
+func _rotatePlayer(delta):
+	rotation = velocity.y * rotationStrength * delta
+	pass
